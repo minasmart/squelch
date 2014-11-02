98599f33d67eaddf1011900d01561bb117d250ee
Mina Smart <mina.smart@thescore.com>
2014-11-02 14:43:29 -0500

    Add license to project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject squelch "0.1.0-SNAPSHOT"
   :description "Squelch: A clojurescript web audio wrapper"
   :url "https://github.com/minasmart/squelch"
+  :license {:name "MIT"
+            :url "http://opensource.org/licenses/MIT"}
 
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2371"]]
