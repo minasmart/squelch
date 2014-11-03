378e9bbdf9733bf264fc1c0817b2e15eec91a22b
Mina Smart <mina.smart@thescore.com>
2014-11-02 22:12:29 -0500

    Define audio context

---
+++ squelch.js
@@ -0,0 +1,4 @@
+goog.addDependency("base.js", ['goog'], []);
+goog.addDependency("../cljs/core.js", ['cljs.core'], ['goog.string', 'goog.object', 'goog.string.StringBuffer', 'goog.array']);
+goog.addDependency("../squelch/core.js", ['squelch.core'], ['cljs.core']);
+goog.addDependency("../squelch/audio_context.js", ['squelch.audio_context'], ['cljs.core']);
