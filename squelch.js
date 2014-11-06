82474af04c33cdc0a1858886dbe81ca55ba307b4
Mina Smart <mina.smart@thescore.com>
2014-11-05 21:05:22 -0500

    Update manifest

--- squelch.js
+++ squelch.js
@@ -1,6 +1,8 @@
 goog.addDependency("base.js", ['goog'], []);
 goog.addDependency("../cljs/core.js", ['cljs.core'], ['goog.string', 'goog.object', 'goog.string.StringBuffer', 'goog.array']);
-goog.addDependency("../squelch/core.js", ['squelch.core'], ['cljs.core']);
+goog.addDependency("../squelch/nodes/script_processor.js", ['squelch.nodes.script_processor'], ['cljs.core']);
 goog.addDependency("../squelch/audio_context.js", ['squelch.audio_context'], ['cljs.core']);
 goog.addDependency("../squelch/audio_param.js", ['squelch.audio_param'], ['cljs.core']);
 goog.addDependency("../squelch/audio_buffer.js", ['squelch.audio_buffer'], ['cljs.core']);
+goog.addDependency("../squelch/nodes/audio_buffer_source.js", ['squelch.nodes.audio_buffer_source'], ['cljs.core']);
+goog.addDependency("../squelch/core.js", ['squelch.core'], ['cljs.core']);
