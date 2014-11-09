2c097b31f500d12267d2444b3319cc68c7c0f18c
Mina Smart <mina.smart@thescore.com>
2014-11-09 13:44:11 -0500

    Rebuild provides file

--- squelch.js
+++ squelch.js
@@ -9,6 +9,7 @@ goog.addDependency("../squelch/audio_buffer.js", ['squelch.audio_buffer'], ['clj
 goog.addDependency("../squelch/nodes/audio_buffer_source.js", ['squelch.nodes.audio_buffer_source'], ['cljs.core']);
 goog.addDependency("../squelch/core.js", ['squelch.core'], ['cljs.core']);
 goog.addDependency("../squelch/nodes/analyser.js", ['squelch.nodes.analyser'], ['cljs.core']);
+goog.addDependency("../squelch/audio_node.js", ['squelch.audio_node'], ['cljs.core']);
 goog.addDependency("../squelch/nodes/delay.js", ['squelch.nodes.delay'], ['cljs.core']);
 goog.addDependency("../squelch/nodes/oscillator.js", ['squelch.nodes.oscillator'], ['cljs.core']);
 goog.addDependency("../squelch/nodes/biquad_filter.js", ['squelch.nodes.biquad_filter'], ['cljs.core']);
