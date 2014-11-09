7397fe805ea51607e4c2576244fce3307235e47a
Mina Smart <mina.smart@thescore.com>
2014-11-08 21:20:44 -0500

    Fix deployment for cljs

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject squelch "0.1.0"
+(defproject squelch "0.1.1"
   :description "Squelch: A clojurescript web audio wrapper"
   :url "https://github.com/minasmart/squelch"
   :license {:name "MIT"
@@ -10,7 +10,7 @@
   :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
   :plugins [[lein-cljsbuild "1.0.4-SNAPSHOT"]]
 
-  :source-paths ["src"]
+  :source-paths ["--cljs-only--"]
 
   :cljsbuild {
     :builds [{:id "squelch"
@@ -21,6 +21,8 @@
                 :output-dir "out"
                 :optimizations :none
                 :source-map true}}]}
+
+  :hooks [leiningen.cljsbuild]
 
   :scm {:name "git"
         :url "https://github.com/minaminamina/squelch"}
